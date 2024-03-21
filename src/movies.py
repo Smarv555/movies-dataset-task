95bfeedfaee2fc818906162d12fef0e9f11a19d6
Stanis <smarv666@gmail.com>
2024-03-21 10:43:58 +0200

    Save to JSON and Logging implementation complete

--- src/movies.py
+++ src/movies.py
@@ -1,21 +1,6 @@
-import ast
 import pandas as pd
 from pandasql import sqldf
-
-
-def read_csv(file_path: str) -> pd.DataFrame:
-    """
-    Read csv file and return a pandas data frame
-    :param file_path: Input file path
-    :return: Pandas data frame
-    """
-    try:
-        df = pd.read_csv(filepath_or_buffer=file_path, header=0, low_memory=False)
-        df = df.convert_dtypes(infer_objects=True)
-    except Exception as error:
-        raise f"Error occurred in read_csv function: {error}"
-
-    return df
+from utils import logger, read_csv, save_to_json
 
 
 class MoviesDataSet:
@@ -30,26 +15,34 @@ class MoviesDataSet:
 
     def get_unique_movies(self) -> pd.DataFrame:
         """
-        Get the number of unique movies in movies dataset
+        Get the number of unique movies in movies dataframe from the class attribute
         :return: Pandas data frame with count of unique movies
         """
+        method_name = self.get_unique_movies.__name__
         movies_table = self.movies_df
         query = """
             SELECT COUNT(DISTINCT id) AS movies_count 
             FROM movies_table;
         """
         try:
+            logger.info(
+                f"Calling {self.__class__.__name__} method {method_name} with query: {query}"
+            )
             df_query_result = sqldf(query)
         except Exception as error:
-            raise f"Error occurred in get_unique_movies method: {error}"
+            error_msg = f"Error occurred in {method_name} method: {error}"
+            logger.error(error_msg)
+            raise error_msg
 
+        logger.info(f"The {method_name} method finished successfully.")
         return df_query_result
 
-    def get_avarage_movie_rating(self) -> pd.DataFrame:
+    def get_average_movie_rating(self) -> pd.DataFrame:
         """
         Get the average rating of all movies
         :return: Pandas data frame with average ratings grouped by movie id
         """
+        method_name = self.get_average_movie_rating.__name__
         movies_table = self.movies_df
         ratings_table = self.ratings_df
         query = """
@@ -62,14 +55,25 @@ class MoviesDataSet:
             GROUP BY id;
         """
         try:
+            logger.info(
+                f"Calling {self.__class__.__name__} method {method_name} with query: {query}"
+            )
             df_query_result = sqldf(query)
         except Exception as error:
-            raise f"Error occurred in get_average_movie_rating method: {error}"
+            error_msg = f"Error occurred in {method_name} method: {error}"
+            logger.error(error_msg)
+            raise error_msg
 
+        logger.info(f"The {method_name} method finished successfully.")
         return df_query_result
 
-    def get_top_5_highest_rated_movies(self):
-        avg_ratings_table = self.get_avarage_movie_rating()
+    def get_top_5_highest_rated_movies(self) -> pd.DataFrame:
+        """
+        Get the top 5 highest rated movies
+        :return: Pandas data frame with the 5 movies with highest average rating
+        """
+        method_name = self.get_top_5_highest_rated_movies.__name__
+        avg_ratings_table = self.get_average_movie_rating()
         query = """
             SELECT 
             id, 
@@ -80,10 +84,16 @@ class MoviesDataSet:
             LIMIT 5;
         """
         try:
+            logger.info(
+                f"Calling {self.__class__.__name__} method {method_name} with query: {query}"
+            )
             df_query_result = sqldf(query)
         except Exception as error:
-            raise f"Error occurred in get_average_movie_rating method: {error}"
+            error_msg = f"Error occurred in {method_name} method: {error}"
+            logger.error(error_msg)
+            raise error_msg
 
+        logger.info(f"The {method_name} method finished successfully.")
         return df_query_result
 
     def get_movies_released_each_year(self) -> pd.DataFrame:
@@ -91,6 +101,7 @@ class MoviesDataSet:
         Get the number of movies released each year
         :return: Pandas data frame with number of movies grouped by released year
         """
+        method_name = self.get_movies_released_each_year.__name__
         movies_table = self.movies_df
         query = """
             SELECT
@@ -102,10 +113,16 @@ class MoviesDataSet:
             ORDER BY movies_released DESC;
         """
         try:
+            logger.info(
+                f"Calling {self.__class__.__name__} method {method_name} with query: {query}"
+            )
             df_query_result = sqldf(query)
         except Exception as error:
-            raise f"Error occurred in get_movies_released_each_year method: {error}"
+            error_msg = f"Error occurred in {method_name} method: {error}"
+            logger.error(error_msg)
+            raise error_msg
 
+        logger.info(f"The {method_name} method finished successfully.")
         return df_query_result
 
     def get_movies_count_by_genre(self) -> pd.DataFrame:
@@ -113,6 +130,7 @@ class MoviesDataSet:
         Get the number of movies for each genre
         :return: Pandas data frame with the number of movies grouped by genre
         """
+        method_name = self.get_movies_count_by_genre.__name__
         movies_table = self.movies_df
         genres_table = self.genres_df
         query = """
@@ -125,28 +143,94 @@ class MoviesDataSet:
             ORDER BY movies_count DESC;
         """
         try:
+            logger.info(
+                f"Calling {self.__class__.__name__} method {method_name} with query: {query}"
+            )
             df_query_result = sqldf(query)
         except Exception as error:
-            raise f"Error occurred in get_movies_count_by_genre method: {error}"
+            error_msg = f"Error occurred in {method_name} method: {error}"
+            logger.error(error_msg)
+            raise error_msg
 
+        logger.info(f"The {method_name} method finished successfully.")
         return df_query_result
+
+    def save_to_json_movies_dataset(self,
+                                    movies_json: str,
+                                    ratings_json: str,
+                                    genres_json: str,
+                                    unique_movies_json: str,
+                                    average_movies_rating_json: str,
+                                    top_5_rated_movies_json: str,
+                                    movies_released_by_year_json: str,
+                                    movies_by_genre_json: str
+                                    ) -> None:
+        """
+        Saving all MoviesDataSet data frames to JSON files
+        :param movies_file_json: movies metadata JSON file path
+        :param ratings_file_json: movie ratings JSON file path
+        :param genres_file_json: movie genres JSON file path
+        :param unique_movies_json: unique movies JSON file path
+        :param average_movies_rating_json: average movie ratings JSON file path
+        :param top_5_rated_movies_json: top 5 rated movies JSON file path
+        :param movies_released_by_year_json: movies released by year JSON file path
+        :param movies_by_genre_json: movies count by genre JSON file path
+        """
+        method_name = self.save_to_json_movies_dataset.__name__
+        try:
+            logger.info(
+                f"Calling {self.__class__.__name__} method {method_name}."
+            )
+            save_to_json(df=self.movies_df, file_path=movies_json)
+            save_to_json(df=self.ratings_df, file_path=ratings_json)
+            save_to_json(df=self.genres_df, file_path=genres_json)
+            save_to_json(df=self.get_unique_movies(), file_path=unique_movies_json)
+            save_to_json(df=self.get_average_movie_rating(), file_path=average_movies_rating_json)
+            save_to_json(df=self.get_top_5_highest_rated_movies(), file_path=top_5_rated_movies_json)
+            save_to_json(df=self.get_movies_released_each_year(), file_path=movies_released_by_year_json)
+            save_to_json(df=self.get_movies_count_by_genre(), file_path=movies_by_genre_json)
+        except Exception as error:
+            error_msg = f"Error occurred in {method_name} method: {error}"
+            logger.error(error_msg)
+            raise error_msg
+
+        logger.info(f"The {method_name} method finished successfully.")
 
 
 if __name__ == "__main__":
-    movies_metadata_file = "../movies_dataset/movies_metadata.csv"
-    ratings_file = "../movies_dataset/ratings_small.csv"
-    genres_file = "../movies_dataset/genres.csv"
+    movies_metadata_csv = "../movies_dataset/csv/movies_metadata.csv"
+    ratings_csv = "../movies_dataset/csv/ratings_small.csv"
+    genres_csv = "../movies_dataset/csv/genres.csv"
 
+    movies_metadata_json = "../movies_dataset/json/movies_metadata.json"
+    ratings_json = "../movies_dataset/json/ratings.json"
+    genres_json = "../movies_dataset/json/genres.json"
+    unique_movies_json = "../movies_dataset/json/unique_movies.json"
+    average_movies_rating_json = "../movies_dataset/json/average_movies_rating.json"
+    top_5_rated_movies_json = "../movies_dataset/json/top_5_rated_movies.json"
+    movies_released_by_year_json = "../movies_dataset/json/movies_released_by_year.json"
+    movies_by_genre_json = "../movies_dataset/json/movies_by_genre.json"
+
+    print("Instantiating MoviesDataSet")
     my_movie_object = MoviesDataSet(
-        movies_file_path=movies_metadata_file,
-        ratings_file_path=ratings_file,
-        genres_file_path=genres_file
+        movies_file_path=movies_metadata_csv,
+        ratings_file_path=ratings_csv,
+        genres_file_path=genres_csv
     )
-    df = my_movie_object.movies_df
-    # print(df.dtypes)
-    print(my_movie_object.get_top_5_highest_rated_movies())
-    # print(df["release_date"].sort_values(ascending=True))
-    # print(my_movie_object.movies_df["release_date"])
-    # print(my_movie_object.get_movies_released_each_year())
+    print("Starting save_to_json_movies_dataset")
+    my_movie_object.save_to_json_movies_dataset(
+        movies_json=movies_metadata_json,
+        ratings_json=ratings_json,
+        genres_json=genres_json,
+        unique_movies_json=unique_movies_json,
+        average_movies_rating_json=average_movies_rating_json,
+        top_5_rated_movies_json=top_5_rated_movies_json,
+        movies_released_by_year_json=movies_released_by_year_json,
+        movies_by_genre_json=movies_by_genre_json
+    )
+    print("save_to_json_movies_dataset finished")
+
+
+
 
 
--- src/utils.py
+++ src/utils.py
@@ -1,29 +1,90 @@
 import ast
+import logging
 import pandas as pd
-from movies import MoviesDataSet
 
-movies_metadata_file = "../movies_dataset/movies_metadata.csv"
-ratings_file = "../movies_dataset/ratings_small.csv"
-genres_file = "../movies_dataset/genres.csv"
-
-movies = MoviesDataSet(movies_file_path=movies_metadata_file, ratings_file_path=ratings_file)
-movies_df = movies.movies_df
-movie_genres_df = pd.DataFrame(
-    {
-        "id": [],
-        "genre_name": []
-    }
+# Logger setup
+logger = logging.getLogger()
+logging.basicConfig(
+    filename="movies_dataset_logs.log", format='%(asctime)s:%(levelname)s:%(message)s', level=logging.DEBUG
 )
 
-for ind in movies_df.index:
-    if movies_df["genres"][ind]:
-        # Converting string to a list
-        genres_list = ast.literal_eval(movies_df["genres"][ind])
-        for genre in genres_list:
-            new_row = {
-                "id": movies_df["id"][ind],
-                "genre_name": genre.get("name")
-            }
-            movie_genres_df = movie_genres_df.append(new_row, ignore_index=True)
+movies_metadata_file = "../movies_dataset/csv/movies_metadata.csv"
+ratings_file = "../movies_dataset/csv/ratings_small.csv"
+genres_file = "../movies_dataset/csv/genres.csv"
 
-movie_genres_df.to_csv(path_or_buf=genres_file, sep=",", header=True, index=False)
+
+def read_csv(file_path: str) -> pd.DataFrame:
+    """
+    Read csv file and return a pandas data frame
+    :param file_path: Input file path
+    :return: Pandas data frame
+    """
+    function_name = read_csv.__name__
+    try:
+        logger.info(
+            f"Calling function {function_name} on file {file_path}."
+        )
+        df = pd.read_csv(filepath_or_buffer=file_path, header=0, low_memory=False)
+        df = df.convert_dtypes(infer_objects=True)
+    except Exception as error:
+        error_msg = f"Error occurred in {function_name} function: {error}"
+        logger.error(error_msg)
+        raise error_msg
+
+    logger.info(
+        f"The {function_name} method finished successfully. Data frame created from CSV file: {file_path}"
+    )
+    return df
+
+
+def save_to_json(df: pd.DataFrame, file_path: str) -> None:
+    """
+    Saving Pandas data frame to a JSON file
+    :param df: Pandas data frame to be saved
+    :param file_path: JSON file path where the data frame will be saved
+    """
+    function_name = save_to_json.__name__
+    try:
+        logger.info(
+            f"Calling function {function_name} on file {file_path}."
+        )
+        df.to_json(path_or_buf=file_path, indent=4)
+    except Exception as error:
+        error_msg = f"Error occurred in {function_name} function: {error}"
+        logger.error(error_msg)
+        raise error_msg
+
+    logger.info(
+        f"The {function_name} method finished successfully. Data frame saved to JSON file: {file_path}"
+    )
+
+
+def generate_genres_df(file_path: str) -> None:
+    """
+
+    :return:
+    """
+    movies_df = read_csv(movies_metadata_file)
+    movie_genres_df = pd.DataFrame(
+        {
+            "id": [],
+            "genre_name": []
+        }
+    )
+
+    for ind in movies_df.index:
+        if movies_df["genres"][ind]:
+            # Converting string to a list
+            genres_list = ast.literal_eval(movies_df["genres"][ind])
+            for genre in genres_list:
+                new_row = {
+                    "id": movies_df["id"][ind],
+                    "genre_name": genre.get("name")
+                }
+                movie_genres_df = movie_genres_df.append(new_row, ignore_index=True)
+
+    movie_genres_df.to_csv(path_or_buf=file_path, sep=",", header=True, index=False)
+
+
+class ConfigParser:
+    pass
